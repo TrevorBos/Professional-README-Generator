7c8b27eb5fb81ce4c19f6071fae823403f4604a6
Trevor Bos <trevor.bos@outlook.com>
2022-06-29 20:09:46 -0400

    Added some comments to better layout how the questions will be presented.

--- index.js
+++ index.js
@@ -5,6 +5,26 @@ const generateMarkdown = require("./utils/generateMarkdown.js");
 // TODO: Create an array of questions for user input
 const questions = [];
 
+// This should be for the project title
+
+
+// This is the description of the project
+
+
+// This is the installation instructions for the project
+
+
+// This is for the usage information for the project
+
+
+// This is for the contribution section of the project
+
+
+// This is for the testing section of the project
+
+
+// This is for the license information for the project
+
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
